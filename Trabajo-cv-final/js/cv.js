45fb9c39fe8dda2c447e9d21567bf3ce69769de7
Marcelo Ocanto <marceloocanto@gmail.com>
2023-03-08 18:16:46 -0300

    Primera entrega del trabajo final integrador.

---
+++ Trabajo-cv-final/js/cv.js
@@ -0,0 +1,8 @@
+// Esta función está aquí para demostrar su uso dentro del código html.
+function saltoLinea() {
+    document.write("<br><br>");
+}
+
+function enviarMensaje() {
+    alert('Con este botón se enviaría un mensaje de correo electrónico, pero no está implementado.');
+}
